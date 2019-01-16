1df708ccee211f4a539b36a608b6c6f6df672680
Daniel Tschinder <daniel@tschinder.de>
2019-01-16 00:31:41 -0800

    Use benchmark.js and update all parsers

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const babylonParse = require('babylon').parse;
-const acornParse = require('acorn').parse;
-const esprimaParse = require('esprima').parse;
-const fs = require('fs');
-const Table  = require("cli-table");
-
+const babylonParse = require("@babel/parser").parse;
+const acornParse = require("acorn").parse;
+const esprimaParse = require("esprima").parse;
+const cherowParse = require("cherow").parse;
+const fs = require("fs");
+const Table = require("cli-table");
+const Benchmark = require("benchmark");
 
 /* START CONFIG */
 
-const ITERATIONS = 20;
-
 const files = [
-  './fixtures/backbone.js',
-  './fixtures/jquery.js',
-  './fixtures/babylon-dist.js',
-  './fixtures/angular.js',
-  './fixtures/react-with-addons.js',
-  './fixtures/ember.debug.js',
+  "./fixtures/ember.debug.js",
+  "./fixtures/jquery.js",
+  "./fixtures/angular.js",
+  "./fixtures/babylon-dist.js",
+  "./fixtures/backbone.js",
+  "./fixtures/react-with-addons.js"
 ];
 
 const plugins = [
   "doExpressions",
   "objectRestSpread",
-  "decorators",
+  ["decorators", { decoratorsBeforeExport: true }],
   "classProperties",
   "exportExtensions",
   "asyncGenerators",
@@ -33,20 +32,20 @@ const plugins = [
   "importMeta",
   "bigInt",
   "jsx",
-  "flow",
-  // "estree"
+  "flow"
+  //"estree"
 ];
 
 const parsers = {
-  babylon: babylonParse,
   acorn: acornParse,
+  babylon: babylonParse,
+  cherow: cherowParse,
   esprima: esprimaParse
 };
 
 /* END CONFIG */
 
 console.log(`Node: ${process.version}`);
-console.log(`ITERATIONS: ${ITERATIONS}`);
 
 const head = ["fixture"];
 for (let i in parsers) {
@@ -57,7 +56,7 @@ function test(parse, plugins, input, iterations) {
   for (let i = 0; i < iterations; i++) {
     parse(input, {
       sourceType: "script",
-      plugins: plugins,
+      //plugins: plugins,
 
       // acorn
       locations: true,
@@ -65,54 +64,71 @@ function test(parse, plugins, input, iterations) {
 
       // esprima
       loc: true,
-      comment: true,
+      comment: true
     });
   }
 }
 
 const table = new Table({
   head,
-  chars: {
-    top: "",
-    "top-mid": "" ,
-    "top-left": "" ,
-    "top-right": "",
-    bottom: "" ,
-    "bottom-mid": "" ,
-    "bottom-left": "" ,
-    "bottom-right": "",
-    left: "",
-    "left-mid": "",
-    mid: "",
-    "mid-mid": "",
-    right: "" ,
-    "right-mid": "",
-    middle: " ",
-  },
   style: {
-    "padding-left": 0,
-    "padding-right": 0,
-    head: ["bold"],
-  },
+    head: ["bold"]
+  }
 });
 
 // warmup cache
-files.forEach((file) => {
-  const code = fs.readFileSync(file, 'utf-8');
+files.forEach(file => {
+  const code = fs.readFileSync(file, "utf-8");
   for (let i in parsers) {
-    test(parsers[i], i === 'babylon' ? plugins: {}, code, 1);
+    test(parsers[i], i === "babylon" ? plugins : {}, code, 1);
   }
 });
 
-files.forEach((file) => {
-  const code = fs.readFileSync(file, 'utf-8');
-  const result = [ file ];
-  for (let i in parsers) {
-    const start = Date.now();
-    test(parsers[i], i === 'babylon' ? plugins: {}, code, ITERATIONS);
-    const avg = (Date.now() - start) / ITERATIONS;
-    result.push(avg.toFixed(1) + "ms");
+files.forEach(file => {
+  if (global.gc) {
+    global.gc();
+  } else {
+    console.warn(
+      "Garbage collection unavailable.  Pass --expose-gc " +
+        "when launching node to enable forced garbage collection."
+    );
   }
+  const code = fs.readFileSync(file, "utf-8");
+  const suite = new Benchmark.Suite(file.replace(/\.\/fixtures\//, ""));
+  for (let i in parsers) {
+    const parser = parsers[i];
+    const options = {
+      sourceType: "script",
+      //plugins: i === 'babylon' ? plugins: {},
+
+      // acorn
+      locations: true,
+      onComment: () => {},
+
+      // esprima
+      loc: true,
+      comment: true
+    };
+
+    suite.add(
+      i,
+      () => {
+        parser(code, options);
+      }
+    );
+  }
+  const result = [suite.name];
+  suite.on("cycle", function(event) {
+    const bench = event.target;
+    const factor = bench.hz < 100 ? 100 : 1;
+    const msg = `${Math.round(bench.hz * factor) /
+      factor} ops/sec ±${Math.round(bench.stats.rme * 100) /
+      100}% (mean ${Math.round(bench.stats.mean * 1000)}ms)`;
+    result.push(msg);
+  });
+
+  console.log(`Running test suite for ${suite.name} ...`);
+  suite.run({ async: false });
   table.push(result);
 });
 
